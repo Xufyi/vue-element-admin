1ca17e966e739ab60af8e1ad25fec68d4ff2e85b
xufengyi <441784941@qq.com>
2020-11-25 12:56:40 +0800

    test

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -209,7 +209,7 @@ export default {
 </script>
 
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
+/* 修复input 背景不协调 和光标变色  */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
 $bg: #283443;
